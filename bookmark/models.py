2a0469f05ba5e2fa3809852f71bec48f96f0d913
kiyongname <stellis001@gmail.com>
2021-03-08 21:57:58 +0900

    홈 url 수정

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+
+
 # Create your models here.
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
@@ -9,7 +11,7 @@ class Bookmark(models.Model):
         ordering = ['pk']
 
     def __str__(self):
-        return "이름 : "+self.site_name+", 주소 : "+self.url
+        return "이름 : " + self.site_name + ", 주소 : " + self.url
 
     def get_absolute_url(self):
         return reverse('detail', args=[str(self.id)])
